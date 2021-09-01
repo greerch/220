cbc555e5a4f6d6b738629a71406104a81d75b525
greerch <greerch@g.cofc.edu>
2021-09-01 13:54:58 -0400

    completed hw 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,14 @@
+"""
+Name: Caroline Greer
+hello.py
+
+Problem: Print "hello, world!" to the screen
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work
+"""
+
+def main():
+    print("hello, world!")
+
+main()
