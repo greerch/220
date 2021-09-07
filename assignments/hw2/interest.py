6aa76193e14273cedd0d0c3cebc953d0211ed64a
greerch <greerch@g.cofc.edu>
2021-09-07 15:54:17 -0400

    completed lab 2

---
+++ assignments/hw2/interest.py
@@ -0,0 +1,16 @@
+"""
+Name: Caroline Greer
+interest.py
+
+Problem: Compute the monthly interest charge on a credit card account
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work
+"""
+
+def main():
+    annual_interest_rate = eval(input("Enter the annual interest rate: "))
+    days_in_billing_period = eval(input("Enter the number of days in the billing period: "))
+    previous_net_balance = eval(input("Enter previous net balance: "))
+    payment_amount = eval(input("Enter the payment amount: "))
+    
--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -1,4 +1,51 @@
+import math
+
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Caroline Greer
+lab2.py
 """
+
+def sum_of_threes():
+    upper_bound = eval(input("Enter the upper bound: "))
+    total = 0
+    for var in range(3, upper_bound+1,3):
+        total =  total + var
+    print(total)
+
+
+def multiplication_table():
+    print(  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10)
+    for row in range (1,11):
+        print(row, end=" ")
+        for col in range (1,11):
+            print(col*row, end=" ")
+        print()
+
+
+def triangle_area():
+    a = eval(input("Enter length of side a"))
+    b = eval(input("Enter length of side b"))
+    c = eval(input("Enter length of side c"))
+    s=(a+b+c)/2
+    area=math.sqrt(s*(s-a)*(s-b)*(s-c))
+    print(area)
+
+
+def sumSquares():
+    lower_bound = eval(input("Enter lower bound: "))
+    upper_bound = eval(input("Enter upper bound: "))
+    total = 0
+    for var in range(lower_bound, upper_bound+1):
+        total = total + var**2
+    print(total)
+
+
+def power():
+    base = eval(input("Enter base number: "))
+    exponent = eval(input("Enter exponent: "))
+    total = 1
+    for var in range(exponent):
+        total = total * base
+    print(base, "^", exponent, "=", total)
+
+
