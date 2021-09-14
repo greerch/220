903a852838a8aea21e0e93cf4af168f9deb00a10
greerch <greerch@g.cofc.edu>
2021-09-13 20:50:06 -0400

    Completed homework 2

--- assignments/hw2/interest.py
+++ assignments/hw2/interest.py
@@ -13,4 +13,14 @@ def main():
     days_in_billing_period = eval(input("Enter the number of days in the billing period: "))
     previous_net_balance = eval(input("Enter previous net balance: "))
     payment_amount = eval(input("Enter the payment amount: "))
-    
+    day_of_cycle = eval(input("Enter the day of billing cycle payment was made: "))
+    step1 = previous_net_balance * days_in_billing_period
+    step2 = payment_amount * (days_in_billing_period - day_of_cycle)
+    step3 = step1 - step2
+    average_daily_balance = step3 / days_in_billing_period
+    monthly_interest_rate = annual_interest_rate / 100.0
+    monthly_interest_charge = average_daily_balance * (monthly_interest_rate / 12.0)
+    print(round(monthly_interest_charge, 2))
+
+if __name__=="__main__":
+    main()
