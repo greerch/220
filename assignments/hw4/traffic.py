801c358685e08fb704e5c9ed60ae95e1ee9d3748
greerch <greerch@g.cofc.edu>
2021-09-28 16:44:23 -0400

    Completed lab 5

---
+++ assignments/hw4/traffic.py
@@ -0,0 +1,22 @@
+"""
+Name: Caroline Greer
+traffic.py
+
+Problem: Calculate and output average number of vehicles traveling down each road
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work
+"""
+
+def main():
+    num_roads = eval(input("How many roads were surveyed? "))
+    cars = 0
+    for days in range(num_roads):
+        enter_days = eval(input("How many days was road" + "days" + "surveyed?" "))
+        for cars in range(enter_cars)
+            enter_cars = eval(input("How many days was road" + "cars" + "surveyed?" "))
+            for 
+
+
+ if __name__ == "__main__":
+    main()
--- labs/lab5/lab5.py
+++ labs/lab5/lab5.py
@@ -1,11 +1,16 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Caroline Greer
+lab5.py
+
+Discription: Solve the problems for lab 5
+
+Certification of authenticity:
+I certify that this assignment is entirely my own work
 """
 
 from graphics import *
 
-
+# OPTIONAL!
 def target():
     win_width = 500
     win_height = 500
@@ -24,7 +29,33 @@ def triangle():
     win = GraphWin("Draw a Triangle", win_width, win_height)
 
     # Add code here to accept the mouse clicks, draw the triangle.
+    point_one = win.getMouse()
+    point_one.draw(win)
+    point_two = win.getMouse()
+    point_two.draw(win)
+    point_three = win.getMouse()
+    point_three.draw(win)
+    triangle_shape = Polygon(point_one, point_two, point_three)
+    triangle_shape.draw(win)
+
     # and display its area in the graphics window.
+    AX = point_one.getX() - point_two.getX()
+    AY = point_one.getY() - point_two.getY()
+    BX = point_two.getX() - point_three.getX()
+    BY = point_two.getY() - point_three.getY()
+    CX = point_three.getX() - point_one.getX()
+    CY = point_three.getY() - point_one.getY()
+    a_length = abs(((AX ** 2) - (AY ** 2) ** 1/2))
+    b_length = abs(((BX ** 2) - (BY ** 2) ** 1/2))
+    c_length = abs(((CX ** 2) - (CY ** 2) ** 1/2))
+    s_value = (a_length + b_length + c_length) / 2
+    area = (s_value * (s_value - a_length) * (s_value - b_length) * (s_value - b_length)) ** 1/2
+    perimeter = a_length + b_length + c_length
+
+    per_text = Text(Point(100, 20), round(perimeter, 5))
+    per_text.draw(win)
+    ar_text = Text(Point(100, 70), round(area, 5))
+    ar_text.draw(win)
 
     # Wait for another click to exit
     win.getMouse()
@@ -51,18 +82,24 @@ def color_shape():
 
     # redTexPt is 50 pixels to the left and forty pixels down from center
     red_text_pt = Point(win_width / 2 - 50, win_height / 2 + 40)
+    red_user_input = Entry(Point(win_width / 2 + 10, win_height / 2 + 40), 10)
+    red_user_input.draw(win)
     red_text = Text(red_text_pt, "Red: ")
     red_text.setTextColor("red")
 
     # green_text_pt is 30 pixels down from red
     green_text_pt = red_text_pt.clone()
     green_text_pt.move(0, 30)
+    green_user_input = Entry(Point(green_text_pt.getX() + 60, green_text_pt.getY()), 10)
+    green_user_input.draw(win)
     green_text = Text(green_text_pt, "Green: ")
     green_text.setTextColor("green")
 
     # blue_text_pt is 60 pixels down from red
     blue_text_pt = red_text_pt.clone()
     blue_text_pt.move(0, 60)
+    blue_user_input = Entry(Point(blue_text_pt.getX() + 60, blue_text_pt.getY()), 10)
+    blue_user_input.draw(win)
     blue_text = Text(blue_text_pt, "Blue: ")
     blue_text.setTextColor("blue")
 
@@ -71,12 +108,65 @@ def color_shape():
     green_text.draw(win)
     blue_text.draw(win)
 
+    for i in range(5):
+        win.getMouse()
+        red = int(red_user_input.getText())
+        green = int(green_user_input.getText())
+        blue = int(blue_user_input.getText())
+        colors = color_rgb(red, green, blue)
+        shape.setFill(colors)
+
     # Wait for another click to exit
     win.getMouse()
     win.close()
 
 
-def main():
+def process_string():
+    string = input("Enter the values to be entered as a string: ")
+    print(string[0])
+    print(string[-1])
+    print(string[1:4])
+    print(string[0] + string[-1])
+    for value in range(len(string)):
+        print(string[value])
+    for i in range(10):
+        print(string[0:3], end=" ")
+    print(len(string))
+
+
+def process_list():
+    pt = Point(5, 10)
+    values = [5, "hi", 2.5, "there", pt, "7.2"]
+    x = values[1] + values[3]
+    print(x)
+    x = values[0] + values[2]
+    print(x)
+    x = values[1] * 5
+    print(x)
+    x = values[2:4]
+    print(x)
+    x = [values[2], values[3], values[0]]
+    print(x)
+    x = [values[2], values[0], values[5]]
+    print(x)
+    x = values[0] + values[2] + eval(values[5])
+    print(x)
+    x = len(values)
+    print(x)
+
+
+def another_series():
+    string_value = int(input("Enter the number of terms to be displayed: "))
+    list = [2, 4, 6]
+    total = 0
+    for i in range(string_value):
+        module = i % 3
+        total = total + list[module]
+        print(list[module], end=" ")
+    print("sum = ", total)
+
+
+    def main():
     # target()
     # triangle()
     # color_shape()
