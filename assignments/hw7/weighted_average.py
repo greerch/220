d5fafcd23e46c198ba43285724f18182dadddb68
greerch <greerch@g.cofc.edu>
2021-10-26 20:25:58 -0400

    Completed HW 7

--- assignments/hw7/weighted_average.py
+++ assignments/hw7/weighted_average.py
@@ -9,38 +9,30 @@ I certify that this assignment is entirely my own work
 """
 
 def weighted_average(in_file_name, out_file_name):
-    in_file_name = input('What file are the grades in?')
-    out_file_name = input('What is the output file?')
-
     in_file = open(in_file_name, 'r')
     out_file = open(out_file_name, 'w')
 
+    class_sum = 0
+    counter = 0
     for line in in_file:
-        lines = line.split()
+        name, numbers = line.split(':')
+        num = numbers.split()
         weight_sum = 0
-        values_sum = 0
-        for i in range(2, len(lines), 2):
-            weight_sum = lines[i] + weight_sum
-        for x in range(3, len(lines), 2):
-            values_sum = lines[x] + values_sum
-
-
-
-    if weight_sum == 100:
-        print (weighted_average, file = out_file, end='/n')
-
-    elif weight_sum >= 100:
-        print("Error: The weights are more than 100", file = out_file, end='/n')
-
-    else:
-        print("Error: The weights are less than 100", file = out_file, end='/n')
+        value = 0
+        for i in range(0, len(num), 2):
+            weight_sum += eval(num[i])
+        if weight_sum < 100:
+            print(name + "'s average: Error: The weights are less than 100.", file=out_file, end='\n')
+        elif weight_sum > 100:
+            print(name + "'s average: Error: The weights are more than 100.", file=out_file, end='\n')
+        else:
+            counter = 1 + counter
+            for i in range(0, len(num), 2):
+                value += eval(num[i + 1]) * eval(num[i])
+            person_avg = value / 100
+            class_sum = class_sum + person_avg
+            print(name + "'s average: " + str(round(person_avg, 1)), file=out_file, end='\n')
+    print("Class average: " + str(round(class_sum/counter, 1)), file=out_file, end='\n')
 
     in_file.close()
     out_file.close()
-
-
-main():
-    weighted_average('grades.txt', 'avg.txt')
-
-if __name__ == '__main__':
-    main
